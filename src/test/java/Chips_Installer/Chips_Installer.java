1ca55124d281f6969252778b7daee8bb6410ea7e
kei <kei@DESKTOP-Kei>
2021-10-25 01:11:44 +0900

    .

--- src/test/java/Chips_Installer/Chips_Installer.java
+++ src/test/java/Chips_Installer/Chips_Installer.java
@@ -12,7 +12,7 @@ import junit.framework.Assert;
 
 public class Chips_Installer extends App_Installer {
 	
-	private static String filePath = "./apk/nugu-friends-engRTG-vc10101-v1.1.1.apk";
+	private static String filePath = "C:\\Users\\kei\\git\\App_Chips_Regression\\apk\\nugu-friends-engRTG-vc10101-v1.1.1.apk";
 	
 	@Test(description = "인스톨러 ")
 	public void Chips_Installer_실행(Method method) throws Exception {
