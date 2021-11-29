c34fa177b197a0b119e2c415c32c84bfe2c632af
kei <kei@DESKTOP-Kei>
2021-11-30 00:50:20 +0900

    .

--- src/test/java/Chips_004_미디어_etc/미디어_12_팟빵.java
+++ src/test/java/Chips_004_미디어_etc/미디어_12_팟빵.java
@@ -390,8 +390,7 @@ public class 미디어_12_팟빵 extends Chips_TestCase {
 		 
 		util.context("NATIVE_APP");
 		test.log(Status.INFO, "W, 구독해줘 - 발화");
-		util.SWFsendPost_playStatus("구독", ServerName, AccessToken, "podcast");
-		util.SWFsendPost_playStatus("구독", ServerName, AccessToken, "podcast");
+		util.SWFsendPost_playStatus("구독 해줘", ServerName, AccessToken, "podcast");
 
 		test.log(Status.INFO, "구독 팟캐스트 TTS 확인");
 		Assert.assertTrue(util.TTS_Assertfunc(nuguqa001, Chips_001, ServerName, Place, "구독 리스트에 추가했습니다."));
