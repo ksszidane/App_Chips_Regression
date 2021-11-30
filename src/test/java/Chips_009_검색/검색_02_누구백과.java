6640cb7da63541729d8b0f222d844e14298d5528
kei <kei@DESKTOP-Kei>
2021-11-30 17:57:15 +0900

    .

--- src/test/java/Chips_009_검색/검색_02_누구백과.java
+++ src/test/java/Chips_009_검색/검색_02_누구백과.java
@@ -276,7 +276,6 @@ public class 검색_02_누구백과 extends Chips_TestCase {
 		util.context("NATIVE_APP");
 		test.log(Status.INFO, "W, 고진감래가 무슨 뜻이야 - 발화");
 		util.SWFsendPost("고진감래가 무슨 뜻이야", ServerName, AccessToken);
-		util.SWFsendPost("고진감래가 무슨 뜻이야", ServerName, AccessToken);
 		
 		util.context("WEBVIEW_com.skt.aidev.nugufriends");
 		util.switchToWindwosURL(xPath.Webview_URL);
